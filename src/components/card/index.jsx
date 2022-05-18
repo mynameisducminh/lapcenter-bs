978420986fd655778af26c3c7ac31f6e89067439
DucMinh <ldmachieve2412@gmail.com>
2022-05-18 21:23:50 +0700

    view list products with mock data and search products

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -4,15 +4,16 @@ import "./styles.scss";
 
 const product1 = require("../../assets/imgs/imgs.jpg");
 
-export default function Card() {
+export default function Card({ product }) {
+  console.log("PRODUCT: ", product);
   return (
     <div className="card-product pb-3 pt-2 my-4 mx-2">
-      <img className="image" src={product1} alt="" />
+      <img className="image" src={product.image} alt="" />
       <div className="info-product mx-3 ">
-        <h5><b>TELEPHONE</b></h5>
-        <p>Hãng: OPPO</p>
-        <p>Chip: helio</p>
-        <p>Giá: 10 man</p>
+        <h5>{product.name}</h5>
+        <p>Hãng: {product.brand}</p>
+        <p>Chip:{product.chip}</p>
+        <p>Giá: {product.price}</p>
       </div>
       <div className="btn-view">
         <Button variant="primary">Xem Sản Phẩm</Button>
