e8fc8a29b15dc116e151fe4a12a0955545de12f2
Duc Minh <ldmachieve2412@gmail.com>
2022-07-31 21:20:48 +0700

    Continue to design buy page

--- src/pages/buy/index.jsx
+++ src/pages/buy/index.jsx
@@ -1,30 +1,90 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.scss";
 import Narbar from "../../components/nabar";
 import { Form, Col, Row, Button } from "react-bootstrap";
+import axios from "axios";
+import Footer from "../../components/footer";
 
 const BuyNow = () => {
   const img =
     "https://fptshop.com.vn/Uploads/Originals/2021/2/22/637496003345935125_dell-vostro-v3500-den-dd.jpg";
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
-    const [phone, setPhone] = useState();
-    const [address, setAddess] = useState();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [phone, setPhone] = useState();
+  const [address, setAddess] = useState();
+  const [quantity, setQuantity] = useState(1);
 
-    const handleChane = (val, field) => {
-        if (field === "name") {
-          setName(val);
-        }
-        if (field === "address") {
-            setAddess(val);
-        }
-        if (field === "email") {
-          setEmail(val);
-        }
-        if (field === "phone") {
-          setPhone(val);
-        }
-      };
+  const [totalprice, setTotalprice] = useState(0);
+  const [product, setProduct] = useState();
+  const [image, setImage] = useState("");
+  const [disable, setDisable] = useState(true);
+
+  const getProductId = () => {
+    // setLoading(true);
+    axios
+      .get(
+        `https://lap-center-v1.herokuapp.com/api/product/getProductById/60c07aaea1364c3894ac0b51`
+      )
+      .then(function (response) {
+        const data = response.data.response;
+        console.log("SUCCESS: ", data);
+        setProduct(data);
+        // setLoading(false);
+        setImage(data.images[0]);
+        setTotalprice(1 * data.price);
+      })
+      .catch(function (error) {
+        // setLoading(false);
+        console.log("ERROR: ", error);
+      })
+      .then(function () {});
+  };
+
+  const handleChane = (val, field) => {
+    if (field === "name") {
+      setName(val);
+    }
+    if (field === "address") {
+      setAddess(val);
+    }
+    if (field === "email") {
+      setEmail(val);
+    }
+    if (field === "phone") {
+      setPhone(val);
+    }
+  };
+
+  const handleChangeQuantity = (val) => {
+    const value = parseInt(val);
+    if (value < 1) {
+      setQuantity(1);
+      setTotalprice(1 * product?.price);
+    } else {
+      setQuantity(val);
+      setTotalprice(val * product?.price);
+    }
+  };
+
+  const handleUpOrDownQuantity = (method) => {
+    if (method === "plus") {
+      setQuantity(quantity + 1);
+      setTotalprice((quantity + 1) * product?.price);
+    } else {
+      if (quantity < 2) {
+        setQuantity(1);
+      } else {
+        setQuantity(quantity - 1);
+        setTotalprice((quantity - 1) * product?.price);
+      }
+    }
+  };
+
+  useEffect(() => {
+    console.log("ham nay chay dau tien");
+    getProductId();
+  }, []);
+
   return (
     <div className="buyContainer">
       <Narbar />
@@ -35,17 +95,39 @@ const BuyNow = () => {
           điền các thông tin dưới đây:
         </span>
         <div className="d-flex justify-content-between mt-4">
-          <img src={img} alt="" width={90} height={70} />
-          <p> Tên sản phẩm</p>
+          <img src={image} alt="" width={90} height={70} />
+          <p className="h5 fw-bold mt-3">{product?.name}</p>
           <div>
-            <Button variant="info" className="mx-2 px-2">
+            <Button
+              variant="info"
+              className="mx-2 px-2"
+              onClick={() => handleUpOrDownQuantity("minus")}
+            >
               <i class="fa-solid fa-minus" />
             </Button>{" "}
-            <input type="NUMBER" className="inp" />
-            <Button variant="info" className="mx-2">
+            <input
+              type="number"
+              className="inp"
+              value={quantity}
+              onChange={(e) => handleChangeQuantity(e.target.value)}
+            />
+            <Button
+              variant="info"
+              className="mx-2"
+              onClick={() => handleUpOrDownQuantity("plus")}
+            >
               <i class="fa-solid fa-plus" />
             </Button>{" "}
           </div>
+        </div>
+        <div className="d-flex justify-content-between">
+          <div />
+          <p className="fw-bold">{product?.price} VND</p>
+        </div>
+        <hr />
+        <div className="d-flex justify-content-between">
+          <p className="fw-bold">Tổng tiền</p>
+          <p className="fw-bold text-danger h6 ">{totalprice} VND</p>
         </div>
 
         <Form>
@@ -114,16 +196,16 @@ const BuyNow = () => {
                 placeholder="Địa Chỉ nhận hàng"
                 value={address}
                 onChange={(e) => handleChane(e.target.value, "address")}
-              
               />
             </Col>
           </Form.Group>
           <div className="d-flex justify-content-center mt-4">
-            <Button variant="success">Đặt hàng</Button>{" "}
+            <Button variant="success" disabled={disable}>Đặt hàng</Button>{" "}
           </div>
         </Form>
       </div>
     </div>
   );
 };
+<Footer />;
 export default BuyNow;
